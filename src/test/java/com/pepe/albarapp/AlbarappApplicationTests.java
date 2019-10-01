816417fc56a2f9f49364fc6f44651b9a9c4e7c71
Pepe <pepevalbe@gmail.com>
2019-10-01 21:50:49 +0200

    Add development profile to test

--- src/test/java/com/pepe/albarapp/AlbarappApplicationTests.java
+++ src/test/java/com/pepe/albarapp/AlbarappApplicationTests.java
@@ -3,14 +3,16 @@ package com.pepe.albarapp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Profile(value = "development")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlbarappApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
