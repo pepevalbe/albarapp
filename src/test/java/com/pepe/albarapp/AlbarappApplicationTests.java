44d9c715f8fccc65a4e069874735f031c3e3f5c1
Pepe <pepevalbe@users.noreply.github.com>
2019-10-02 16:01:31 +0200

    Update AlbarappApplicationTests.java

--- src/test/java/com/pepe/albarapp/AlbarappApplicationTests.java
+++ src/test/java/com/pepe/albarapp/AlbarappApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@Profile(value = "development")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlbarappApplicationTests {
