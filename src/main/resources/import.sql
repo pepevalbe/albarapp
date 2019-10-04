ab1f4bcb945d2ac375a3eaac6d58350db5dd2c4a
pepevalbe <pepevalbe@gmail.com>
2019-10-04 21:08:24 +0200

    Update properties

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,16 +1,16 @@
 INSERT INTO product(id, code, name, factory_price, tax) VALUES
-	(1, 1, 'Bandeja - Huevo XL - 20uds', 3.6, 0),
-	(2, 2, 'Bandeja - Huevo L - 30uds', 5.1, 0),
-	(3, 3, 'Bandeja - Huevo M - 30uds', 4.8, 0),
-	(4, 4, 'Bandeja - Huevo S - 30uds', 4.5, 0),
-	(5, 5, 'Estuche - Huevo S - 10uds', 1.5, 0),
-	(6, 6, 'Estuche - Huevo S - 6uds', 0.9, 0),
+	(1, 1, 'Huevo XL', 0.15, 0),
+	(2, 2, 'Huevo L', 0.13, 0),
+	(3, 3, 'Huevo M', 0.11, 0),
+	(4, 4, 'Huevo S', 0.09, 0),
+	(5, 5, 'Huevo M - Estuche 10uds', 0.13, 0),
+	(6, 6, 'Huevo M - Estuche 6uds', 4.5, 0),
 	(7, 7, 'Gallina', 4, 6.5);
 	
 INSERT INTO customer(id, fiscal_id, code, name, alias, phone_number, email, address, province) VALUES
     (1, 'H35083187', 1, 'C.C Yumbo Centrum', 'jumbo', '928764196', 'info@yumbocentrum.com', 'Av. Estados Unidos, 54, 35100 Maspalomas, Las Palmas', 'Las Palmas'),
-    (2, 'aldeaCIF', 2, 'Bar Lauremar', 'aldea', '928892141', null, 'Av. los Cardones, 21, 35470 La Aldea de San Nicolas de Tolentino', 'Las Palmas'),
-    (3, 'locoDNI', 3, 'Loco de Vecindario', 'loco', null, null, null, null);
+    (2, 'B00000000', 2, 'Bar Lauremar', 'aldea', '928892141', null, 'Av. los Cardones, 21, 35470 La Aldea de San Nicolas de Tolentino', 'Las Palmas'),
+    (3, '80000000G', 3, 'Loco de Vecindario', 'loco', null, null, null, null);
 
 INSERT INTO customer_product_price(id, offered_price, customer_id, product_id) VALUES
     (1, 3.6, 1, 1),
