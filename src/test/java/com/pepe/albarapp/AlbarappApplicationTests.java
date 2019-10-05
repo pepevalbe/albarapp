63c98326d42dc92816117406e9b3c2d38f59f154
pepevalbe <pepevalbe@gmail.com>
2019-10-05 12:02:44 +0200

    Disable tests

--- src/test/java/com/pepe/albarapp/AlbarappApplicationTests.java
+++ src/test/java/com/pepe/albarapp/AlbarappApplicationTests.java
@@ -3,15 +3,14 @@ package com.pepe.albarapp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlbarappApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
