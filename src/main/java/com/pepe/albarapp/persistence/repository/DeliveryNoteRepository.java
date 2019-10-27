54a98d2f59c7df4dd866a9fdc94d5b3797e3840e
Pepe <pepevalbe@gmail.com>
2019-10-27 19:23:03 +0100

    Add custom query method

--- src/main/java/com/pepe/albarapp/persistence/repository/DeliveryNoteRepository.java
+++ src/main/java/com/pepe/albarapp/persistence/repository/DeliveryNoteRepository.java
@@ -4,7 +4,11 @@ import com.pepe.albarapp.persistence.domain.DeliveryNote;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:8080")
 //@RepositoryRestResource(excerptProjection = DeliveryNoteView.class)
 public interface DeliveryNoteRepository extends CrudRepository<DeliveryNote, Long> {
+
+	List<DeliveryNote> findByIssuedTimestampGreaterThan(long issuedTimestamp);
 }
