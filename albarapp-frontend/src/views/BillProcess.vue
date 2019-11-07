6e8eceea7fd276788de6c8d419b8f8c6bf480b5c
Omar <oespino88@gmail.com>
2019-11-07 23:13:37 +0100

    Minor improvements in bill process

--- albarapp-frontend/src/views/BillProcess.vue
+++ albarapp-frontend/src/views/BillProcess.vue
@@ -5,18 +5,18 @@
       <v-row justify="center">
         <v-col cols="12" md="3">
           <v-text-field
-            v-model="customerCodeFrom"
+            v-model="form.customerCodeFrom"
             type="number"
             :counter="5"
             autofocus
-            :rules="[codeFromRules]"
+            :rules="codeFromRules"
             label="Código desde *"
             required
           ></v-text-field>
         </v-col>
         <v-col cols="12" md="3">
           <v-text-field
-            v-model="customerCodeTo"
+            v-model="form.customerCodeTo"
             :counter="5"
             :rules="codeToRules"
             label="Código hasta *"
@@ -26,38 +26,105 @@
       </v-row>
       <v-row justify="center">
         <v-col cols="12" md="3">
-          <v-text-field
-            v-model="dateFrom"
-            type="text"
-            :rules="dateFromRules"
-            label="Fecha desde (dd/mm/aaaa) *"
-            required
-          ></v-text-field>
+          <v-menu
+            ref="menuDatePicker"
+            v-model="menuDateFromPicker"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="form.dateFromFormatted"
+                ref="dateText"
+                label="Fecha desde"
+                hint="Formato: ddMMaaaa"
+                :rules="dateFromRules"
+                persistent-hint
+                @focus="$event.target.select()"
+                prepend-icon="mdi-calendar"
+                @blur="parseDateFromText()"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="form.dateFrom"
+              no-title
+              @input="parseDateFromPick()"
+              locale="es-ES"
+              first-day-of-week="1"
+            ></v-date-picker>
+          </v-menu>
         </v-col>
         <v-col cols="12" md="3">
-          <v-text-field
-            v-model="dateTo"
-            type="text"
-            :rules="dateToRules"
-            label="Fecha hasta (dd/mm/aaaa) *"
-            required
-          ></v-text-field>
+          <v-menu
+            ref="menuDatePicker"
+            v-model="menuDateToPicker"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="form.dateToFormatted"
+                ref="dateText"
+                label="Fecha hasta"
+                hint="Formato: ddMMaaaa"
+                :rules="dateToRules"
+                persistent-hint
+                @focus="$event.target.select()"
+                prepend-icon="mdi-calendar"
+                @blur="parseDateToText()"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="form.dateTo"
+              no-title
+              @input="parseDateToPick()"
+              locale="es-ES"
+              first-day-of-week="1"
+            ></v-date-picker>
+          </v-menu>
         </v-col>
       </v-row>
       <v-row justify="center">
         <v-col cols="12" md="3">
-          <v-text-field
-            v-model="issuedDate"
-            type="text"
-            :rules="issuedDateRules"
-            label="Fecha emisión (dd/mm/aaaa) *"
-            required
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row justify="center">
-        <v-col cols="12" md="6">
-          <v-alert v-show="alertShow" type="warning">{{alertMessage}}</v-alert>
+          <v-menu
+            ref="menuDatePicker"
+            v-model="menuIssuedDatePicker"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="form.issuedDateFormatted"
+                ref="dateText"
+                label="Fecha de emisión"
+                hint="Formato: ddMMaaaa"
+                :rules="issuedDateRules"
+                persistent-hint
+                @focus="$event.target.select()"
+                prepend-icon="mdi-calendar"
+                @blur="parseIssuedDateText()"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="form.issuedDate"
+              no-title
+              @input="parseIssuedDatePick()"
+              locale="es-ES"
+              first-day-of-week="1"
+            ></v-date-picker>
+          </v-menu>
         </v-col>
       </v-row>
     </v-form>
@@ -70,6 +137,10 @@
       >Facturar</v-btn>
       <v-btn to="/invoice-list/">Volver</v-btn>
     </v-row>
+    <v-snackbar v-model="snackbar">
+      Creadas {{numberInvoicesCreated}} facturas correctamente
+      <v-btn color="success" text @click="snackbar = false">Cerrar</v-btn>
+    </v-snackbar>
   </v-container>
 </template>
 
@@ -80,13 +151,21 @@ export default {
   name: "BillProcess",
   data: () => ({
     form: {
-      valid: false
+      valid: false,
+      dateFrom: "",
+      dateFromFormatted: "",
+      dateTo: "",
+      dateToFFormatted: "",
+      issuedDate: "",
+      issuedDateFormatted: "",
+      customerCodeFrom: "",
+      customerCodeTo: ""
     },
-    customerCodeFrom: "",
-    customerCodeTo: "",
-    dateFrom: "",
-    dateTo: "",
-    issuedDate: "",
+    menuDateFromPicker: false,
+    menuDateToPicker: false,
+    menuIssuedDatePicker: false,
+    snackbar: false,
+    numberInvoicesCreated: "",
     codeFromRules: [
       v => !!v || "El código es obligatorio",
       v =>
@@ -101,19 +180,69 @@ export default {
     ],
     dateFromRules: [v => !!v || "La fecha desde es obligatoria"],
     dateToRules: [v => !!v || "La fecha hasta es obligatoria"],
-    issuedDateRules: [v => !!v || "La fecha de emisión es obligatoria"],
-    alertMessage: "",
-    alertShow: false
+    issuedDateRules: [v => !!v || "La fecha de emisión es obligatoria"]
   }),
   methods: {
     async createInvoices() {
-      InvoiceService.createList(
-        this.customerCodeFrom,
-        this.customerCodeTo,
-        this.$moment(this.dateFrom, "DD/MM/YYYY").format("x"),
-        this.$moment(this.dateTo, "DD/MM/YYYY").format("x"),
-        this.$moment(this.issuedDate, "DD/MM/YYYY").format("x")
+      await InvoiceService.createList(
+        this.form.customerCodeFrom,
+        this.form.customerCodeTo,
+        this.$moment(this.form.dateFromFormatted, "DD/MM/YYYY").format("x"),
+        this.$moment(this.form.dateToFormatted, "DD/MM/YYYY").format("x"),
+        this.$moment(this.form.issuedDateFormatted, "DD/MM/YYYY").format("x")
+      ).then(values => {
+        this.numberInvoicesCreated = values.length;
+        this.snackbar = true;
+        this.$refs.form.reset();
+      });
+    },
+    parseDateFromPick() {
+      var moment = this.$moment(this.form.dateFrom, "YYYY-MM-DD", true);
+      this.form.dateFromFormatted = moment.format("DD/MM/YYYY");
+      this.menuDateFromPicker = false;
+    },
+    parseDateFromText() {
+      var moment = this.$moment(
+        this.form.dateFromFormatted,
+        ["DDMMYYYY", "DD/MM/YYYY"],
+        true
       );
+      if (moment.isValid()) {
+        this.form.dateFrom = moment.format("YYYY-MM-DD");
+        this.form.dateFromFormatted = moment.format("DD/MM/YYYY");
+      }
+    },
+    parseDateToPick() {
+      var moment = this.$moment(this.form.dateTo, "YYYY-MM-DD", true);
+      this.form.dateToFormatted = moment.format("DD/MM/YYYY");
+      this.menuDateToPicker = false;
+    },
+    parseDateToText() {
+      var moment = this.$moment(
+        this.form.dateToFormatted,
+        ["DDMMYYYY", "DD/MM/YYYY"],
+        true
+      );
+      if (moment.isValid()) {
+        this.form.dateTo = moment.format("YYYY-MM-DD");
+        this.form.dateToFormatted = moment.format("DD/MM/YYYY");
+      }
+    },
+    parseIssuedDatePick() {
+      var moment = this.$moment(this.form.issuedDate, "YYYY-MM-DD", true);
+      this.form.issuedDateFormatted = moment.format("DD/MM/YYYY");
+      this.menuIssuedDatePicker = false;
+    },
+    parseIssuedDateText() {
+      var moment = this.$moment(
+        this.form.issuedDateFormatted,
+        ["DDMMYYYY", "DD/MM/YYYY"],
+        true
+      );
+      if (moment.isValid()) {
+        this.form.issuedDate = moment.format("YYYY-MM-DD");
+        this.form.issuedDateFormatted = moment.format("DD/MM/YYYY");
+      }
     }
   }
 };
