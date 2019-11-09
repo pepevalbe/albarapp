a5946dbacfa649f466ce55dca506e766da2aa8d7
pepevalbe <pepevalbe@gmail.com>
2019-11-09 21:01:55 +0100

    Add resource not found handler

--- src/main/java/com/pepe/albarapp/api/error/ApiExceptionHandler.java
+++ src/main/java/com/pepe/albarapp/api/error/ApiExceptionHandler.java
@@ -2,6 +2,7 @@ package com.pepe.albarapp.api.error;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -26,6 +27,11 @@ public class ApiExceptionHandler {
 	ResponseEntity<Object> handleAccessDeniedException(AccessDeniedException ex) {
 		logger.error(ApiError.ApiError005.toString());
 		return ResponseEntity.status(ApiError.ApiError005.httpCode).body(ApiError.ApiError005);
+	}
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
+		throw ex;
 	}
 
 	@ExceptionHandler(Exception.class)
