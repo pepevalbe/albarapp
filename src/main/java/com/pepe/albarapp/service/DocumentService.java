8fe573798241cffe25a7569d4c6cfd36bbdd75b0
Pepe <pepevalbe@gmail.com>
2019-11-24 21:22:53 +0100

    Polish

--- src/main/java/com/pepe/albarapp/service/DocumentService.java
+++ src/main/java/com/pepe/albarapp/service/DocumentService.java
@@ -79,26 +79,4 @@ public class DocumentService {
 		zipOut.close();
 	}
 
-	public void example() throws IOException {
-		List<String> srcFiles = Arrays.asList("test1.txt", "test2.txt");
-		FileOutputStream fos = new FileOutputStream("multiCompressed.zip");
-
-		ZipOutputStream zipOut = new ZipOutputStream(fos);
-
-		for (String srcFile : srcFiles) {
-			File fileToZip = new File(srcFile);
-			FileInputStream fis = new FileInputStream(fileToZip);
-			ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
-			zipOut.putNextEntry(zipEntry);
-
-			byte[] bytes = new byte[1024];
-			int length;
-			while ((length = fis.read(bytes)) >= 0) {
-				zipOut.write(bytes, 0, length);
-			}
-			fis.close();
-		}
-		zipOut.close();
-		fos.close();
-	}
 }
