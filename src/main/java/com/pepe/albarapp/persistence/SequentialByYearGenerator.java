54868728519d06f7fc447af3f0ee4de0de9ebb59
Omar <oespino88@gmail.com>
2019-12-04 08:31:54 +0100

    Fix: use issuedTimestamp year to generate ID for invoices and deliveryNotes instead of local current year

--- src/main/java/com/pepe/albarapp/persistence/SequentialByYearGenerator.java
+++ src/main/java/com/pepe/albarapp/persistence/SequentialByYearGenerator.java
@@ -4,8 +4,13 @@ import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
+import com.pepe.albarapp.persistence.domain.Invoice;
+import com.pepe.albarapp.persistence.domain.DeliveryNote;
+
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.Instant;
 
 public class SequentialByYearGenerator implements IdentifierGenerator {
 
@@ -14,12 +19,23 @@ public class SequentialByYearGenerator implements IdentifierGenerator {
 
 		String idColumnName = session.getEntityPersister(obj.getClass().getName(), obj).getIdentifierPropertyName();
 		String tableName = obj.getClass().getSimpleName();
-		String query = String.format("select max(%s) from %s", idColumnName, tableName);
+		LocalDate localDate;
 
+		if (obj instanceof Invoice) {
+			long issuedTimestamp = ((Invoice) obj).getIssuedTimestamp();
+			localDate = Instant.ofEpochMilli(issuedTimestamp).atZone(ZoneId.systemDefault()).toLocalDate();
+		} else {
+			long issuedTimestamp = ((DeliveryNote) obj).getIssuedTimestamp();
+			localDate = Instant.ofEpochMilli(issuedTimestamp).atZone(ZoneId.systemDefault()).toLocalDate();
+		}
 		//Get first Id of current year
-		long firstId = LocalDate.now().getYear() * 100000L;
+		long firstId = localDate.getYear() * 100000L;
 
-		// Get latest Id
+		//Get first Id of next year
+		long firstIdNextYear = (localDate.getYear() + 1) * 100000L;
+
+		// Get latest Id of current year
+		String query = String.format("select max(%s) from %s where id < %s", idColumnName, tableName, Long.toString(firstIdNextYear));
 		Long max = (Long) session.createQuery(query).getSingleResult();
 
 		// Empty table
