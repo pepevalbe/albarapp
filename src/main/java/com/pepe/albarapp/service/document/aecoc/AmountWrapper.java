f97c12373bcb673e2092edb56ca0cdb788cf1abc
Omar <oespino88@gmail.com>
2020-02-21 22:47:51 +0100

    Fix formatting amount in XML EDI

--- src/main/java/com/pepe/albarapp/service/document/aecoc/AmountWrapper.java
+++ src/main/java/com/pepe/albarapp/service/document/aecoc/AmountWrapper.java
@@ -1,6 +1,9 @@
 package com.pepe.albarapp.service.document.aecoc;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import lombok.Data;
 
@@ -8,8 +11,12 @@ import lombok.Data;
 public class AmountWrapper {
 
 	private Amount amount;
+	private static final NumberFormat NUMBER_FORMAT = NumberFormat.getInstance(new Locale("en", "EN"));
 
 	public AmountWrapper(BigDecimal amount) {
-		this.amount = new Amount(amount.toString());
+		NUMBER_FORMAT.setMaximumFractionDigits(3);
+		NUMBER_FORMAT.setMinimumFractionDigits(0);
+		NUMBER_FORMAT.setGroupingUsed(false);
+		this.amount = new Amount(NUMBER_FORMAT.format(amount));
 	}
 }
