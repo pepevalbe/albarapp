113b5fc3099a900fa914dbc772af7216742c5810
Pepe <pepevalbe@gmail.com>
2020-02-23 10:47:12 +0100

    Add most billed customer statistic

--- src/main/java/com/pepe/albarapp/service/StatisticsService.java
+++ src/main/java/com/pepe/albarapp/service/StatisticsService.java
@@ -1,13 +1,12 @@
 package com.pepe.albarapp.service;
 
-import com.pepe.albarapp.persistence.repository.*;
 import com.pepe.albarapp.service.dto.StatisticsDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -15,24 +14,26 @@ import java.util.List;
 public class StatisticsService {
 
 	private static final String TOTAL_CUSTOMERS = "Total clientes";
+	private static final String TOTAL_CUSTOMERS_QUERY = "select count(ID) from CUSTOMER";
 	private static final String TOTAL_INVOICES = "Total facturas";
+	private static final String TOTAL_INVOICES_QUERY = "select count(ID) from INVOICE";
 	private static final String TOTAL_DELIVERY_NOTES = "Total albaranes";
+	private static final String TOTAL_DELIVERY_NOTES_QUERY = "select count(ID) from DELIVERY_NOTE";
+	private static final String MOST_BILLED_CUSTOMER = "Cliente mayor facturación";
+	private static final String MOST_BILLED_CUSTOMER_QUERY = "select alias, SUM(quantity*price) as total FROM delivery_note INNER JOIN customer INNER JOIN delivery_note_item GROUP BY alias ORDER BY total DESC LIMIT 1";
 
 	@Autowired
-	private CustomerRepository customerRepository;
-
-	@Autowired
-	private InvoiceRepository invoiceRepository;
-
-	@Autowired
-	private DeliveryNoteRepository deliveryNoteRepository;
+	JdbcTemplate jdbcTemplate;
 
 	public List<StatisticsDto> getStatistics() {
 
 		List<StatisticsDto> statistics = new ArrayList<>();
-		statistics.add(new StatisticsDto(TOTAL_CUSTOMERS, String.valueOf(customerRepository.count())));
-		statistics.add(new StatisticsDto(TOTAL_INVOICES, String.valueOf(invoiceRepository.count())));
-		statistics.add(new StatisticsDto(TOTAL_DELIVERY_NOTES, String.valueOf(deliveryNoteRepository.count())));
+
+		statistics.add(new StatisticsDto(TOTAL_CUSTOMERS, jdbcTemplate.queryForObject(TOTAL_CUSTOMERS_QUERY, String.class)));
+		statistics.add(new StatisticsDto(TOTAL_INVOICES, jdbcTemplate.queryForObject(TOTAL_INVOICES_QUERY, String.class)));
+		statistics.add(new StatisticsDto(TOTAL_DELIVERY_NOTES, jdbcTemplate.queryForObject(TOTAL_DELIVERY_NOTES_QUERY, String.class)));
+		List<Object> mostBilledResult = new ArrayList<>(jdbcTemplate.queryForMap(MOST_BILLED_CUSTOMER_QUERY).values());
+		statistics.add(new StatisticsDto(MOST_BILLED_CUSTOMER, String.format("%s (%s€)", mostBilledResult.get(0), mostBilledResult.get(1))));
 
 		return statistics;
 	}
