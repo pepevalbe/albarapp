6b55e8835d602d1e1bda1b1f912b6f796c9e7eac
Pepe <pepevalbe@gmail.com>
2020-03-08 23:22:47 +0100

    Improve docker building with versioning

--- scripts/build_app_docker.sh
+++ scripts/build_app_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Get version from pom, build jar and build docker image
 cd /home/ec2-user/albarapp
+version=$(./mvnw -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 ./mvnw clean install
-docker build -t albarapp/albarapp .
+docker build -t albarapp/albarapp:$version .
--- scripts/start_app_docker.sh
+++ scripts/start_app_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Stop all docker containers, remove all stopped docker images, create albarapp container
+# Get version from pom, stop all docker containers and create albarapp container
+cd /home/ec2-user/albarapp
+version=$(./mvnw -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 docker stop $(docker ps -a -q)
-docker system prune --force
-docker run -d -p 80:8085 --env-file /home/ec2-user/env.albarapp albarapp/albarapp
+docker run -d -p 80:8085 --env-file /home/ec2-user/env.albarapp albarapp/albarapp:$version
