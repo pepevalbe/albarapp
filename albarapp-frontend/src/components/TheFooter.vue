bd4d050f2110498d7f5e9d3c9720f61fe3682867
oespino <oespino88@gmail.com>
2021-03-30 18:29:32 +0200

    Fix footer year.

--- albarapp-frontend/src/components/TheFooter.vue
+++ albarapp-frontend/src/components/TheFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer app dark color="grey darken-3">
     <div class="flex-grow-1"></div>
-    <span class="text-right">&copy; 2021</span>
+    <span class="text-right">&copy; {{$moment().year()}}</span>
   </v-footer>
 </template>
 
