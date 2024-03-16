4786fd428bf86cc3806c364e745c81544eae18c6
oespino <oespino88@gmail.com>
2024-03-16 13:58:18 +0100

    Add Origin CORS

--- src/main/java/com/pepe/albarapp/AlbarappApplication.java
+++ src/main/java/com/pepe/albarapp/AlbarappApplication.java
@@ -20,7 +20,7 @@ public class AlbarappApplication implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("*");
+		registry.addMapping("/**").allowedOrigins("http://localhost:8080", "https://albarapp.avicolasanantonio.com").allowedMethods("*");
 	}
 
 	@Bean
